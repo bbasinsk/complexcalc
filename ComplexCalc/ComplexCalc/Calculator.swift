64fac395de65661408bdfd1f1fd90b77b3148cfd
Ben Basinski <benbasinski97@gmail.com>
2019-01-15 19:37:07 -0800

    create Calculator method headers

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,81 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    
+    // two args
+    func add(lhs: Int, rhs: Int) -> Int {
+        return 0
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return 0
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return 0
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return 0
+    }
+    
+    
+    // with op
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return 0
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        return 0
+    }
+    
+    
+    // args
+    func add(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    func subtract(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    func multiply(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    func divide(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    func mathOp(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    func count(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    func avg(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    
+    // tuples
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (0, 0)
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (0, 0)
+    }
+    
+    
+    // dictionaries
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": 0]
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": 0]
+    }
 }
